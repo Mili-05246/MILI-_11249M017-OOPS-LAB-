32bfedf94b918f1bdc05cdae7bf5a9d6efa9b3f8
Mili-05246 <milisignaturedomen@gmail.com>
2025-11-11 00:02:04 +0530

    Implement Student class with various constructors
    Added a class 'Student' demonstrating different types of constructors.

--- UNIT 2/CONSTRUCTOR_with_all_its_types.cpp
+++ UNIT 2/CONSTRUCTOR_with_all_its_types.cpp
@@ -1,1 +1,58 @@
 
+#include <iostream>
+using namespace std;
+
+class Student {
+    int roll;
+    string name;
+    float marks;
+
+public:
+    // 1. Default Constructor
+    Student() {
+        roll = 0;
+        name = "Unknown";
+        marks = 0.0;
+        cout << "Default Constructor Called" << endl;
+    }
+
+    // 2. Parameterized Constructor
+    Student(int r, string n, float m) {
+        roll = r;
+        name = n;
+        marks = m;
+        cout << "Parameterized Constructor Called" << endl;
+    }
+
+    // 3. Copy Constructor
+    Student(const Student &s) {
+        roll = s.roll;
+        name = s.name;
+        marks = s.marks;
+        cout << "Copy Constructor Called" << endl;
+    }
+
+    // Function to display data
+    void display() {
+        cout << "Roll: " << roll << ", Name: " << name << ", Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    cout << "--- Demonstrating Constructors in C++ ---" << endl;
+
+    
+    Student s1;
+    s1.display();
+
+    // Parameterized constructor
+    Student s2(101, "Rahul", 88.5);
+    s2.display();
+
+    // Copy constructor
+    Student s3 = s2;
+    s3.display();
+
+    return 0;
+}
+
